3d590e179c39a86f3ca4f9f3aae47402d4720f6b
bsp <brendan.sapience@gmail.com>
2015-10-22 00:22:13 -0400

    adding more methods for activities

--- src/com/automic/objects/ActivityWindow.java
+++ src/com/automic/objects/ActivityWindow.java
@@ -8,6 +8,12 @@ import com.uc4.api.Task;
 import com.uc4.api.TaskFilter;
 import com.uc4.communication.Connection;
 import com.uc4.communication.requests.ActivityList;
+import com.uc4.communication.requests.AddComment;
+import com.uc4.communication.requests.CancelTask;
+import com.uc4.communication.requests.DeactivateTask;
+import com.uc4.communication.requests.ModifyTaskState;
+import com.uc4.communication.requests.RestartTask;
+import com.uc4.communication.requests.SuspendTask;
 
 public class ActivityWindow extends ObjectTemplate{
 
@@ -20,19 +26,162 @@ public class ActivityWindow extends ObjectTemplate{
 	}
 	
 	// return the content of the activity window
-		public List<Task> getActivityWindowContent(TaskFilter taskFilter) throws IOException {		
-			ActivityList list = new ActivityList(taskFilter);
-			connection.sendRequestAndWait(list);		
-			List<Task> tasks = new ArrayList<Task>();
-			for (Task t : list) {
-				tasks.add(t);
-			}
-			return tasks;
+	public List<Task> getActivityWindowContent(TaskFilter taskFilter) throws IOException {		
+		ActivityList list = new ActivityList(taskFilter);
+		connection.sendRequestAndWait(list);		
+		List<Task> tasks = new ArrayList<Task>();
+		for (Task t : list) {
+			tasks.add(t);
 		}
+		return tasks;
+	}
 		
-		// return the content of the activity window
-		public List<Task> getActivityWindowContent() throws IOException {		
-			TaskFilter taskFilter = new TaskFilter();
-			return getActivityWindowContent(taskFilter);
+	// return the content of the activity window
+	public List<Task> getActivityWindowContent() throws IOException {		
+		TaskFilter taskFilter = new TaskFilter();
+		return getActivityWindowContent(taskFilter);
+	}
+		
+	public boolean deactivateTask(int RunID, boolean force) throws IOException {		
+		DeactivateTask req = new DeactivateTask(RunID, force); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
 		}
+		return true;
+	}
+	
+	public boolean restartTask(int RunID) throws IOException {		
+		RestartTask req = new RestartTask(RunID); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean cancelTask(int RunID,boolean Recursive) throws IOException {		
+		CancelTask req = new CancelTask(RunID, Recursive); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
+		}
+		return true;
+	}
+	public boolean addComment(int RunID,String comment) throws IOException {		
+		AddComment req = new AddComment(RunID, comment); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean stopTask(int RunID,boolean force) throws IOException {		
+		SuspendTask req = new SuspendTask(RunID, force); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean modifyStatus(int RunID, int oldStatus, int newStatus) throws IOException {		
+		ModifyTaskState req = new ModifyTaskState(RunID, oldStatus, newStatus); // force
+		connection.sendRequestAndWait(req);		
+		if (req.getMessageBox() != null) {
+			System.out.println(" -- "+req.getMessageBox().getText().toString().replace("\n", ""));
+			return false;
+		}
+		return true;
+		/* 
+		Status	Description
+		1300	Preparing
+		1510	Transferred
+		1520	Ready for transfer (before Connect)
+		1521	Ready to be transferred
+		1529	Ready for generation
+		1530	Ready for start
+		1531	To be called
+		1540	Start initiated
+		1541	Checking
+		1542	Calling
+		1543	Unknown
+		1544	Inconsistent
+		1545	Started
+		1546	Connecting
+		1550	Active
+		1551	Transferring
+		1552	Called
+		1553	Accepted
+		1554	Sampling files
+		1556	Escalated
+		1560	JobPlan blocked
+		1561	Stopped - system-wide stop of automatic processing
+		1562	HELD - manual stop was set
+		1563	Stopped - automatic processing was stopped.
+		1565	Finished sending task
+		1569	Skipping Sync
+		1570	Skipping
+		1571	Canceling
+		1572	Generating
+		1573	Generated
+		1574	Post processing
+		1575	Ending
+		1599	Alive user view
+		1600	Internal checking
+		1686	Waiting for start of host-group container.
+		1687	Waiting for end of parallel host-group tasks
+		1688	Waiting for the host of a host group
+		1689	Waiting for resource (max. filetransfers exceeded
+		1690	Waiting for external dependency
+		1691	Waiting for remote system
+		1692	Waiting for remote SYNC
+		1693	Waiting for manual release
+		1694	Waiting for resource (max. jobs exceeded)
+		1695	Waiting for restart time
+		1696	Waiting for host
+		1697	Waiting for SYNC
+		1698	Waiting for start time
+		1700	Waiting for precondition
+		1701	Sleeping
+		1702	Not yet called
+		1709	Waiting for end of parallel task(s)
+		1710	Registered
+		1800	ENDED_NOT_OK - aborted
+		1801	ENDED_NOT_OK - aborted because of SYNC condition
+		1802	ENDED_JP_ABEND - not executed due to abnormal JobPlan end
+		1810	ENDED_VANISHED - disappeared
+		1815	ENDED_LOST - ended undefined (host terminated prematurely)
+		1820	FAULT_OTHER - Start impossible. Other error
+		1821	FAULT_NO_HOST - Start impossible. Cannot reach host.
+		1822	FAULT_ALREADY_RUNNING - task is already running
+		1823	FAULT_POST_PROCESSING - error in post processing.
+		1850	ENDED_CANCEL - manually canceled
+		1851	ENDED_JP_CANCEL - JobPlan canceled manually
+		1853	ENDED_QUEUE_CANCEL
+		1854	ENDED_CONTAINER_CANCEL
+		1856	ENDED_ESCALATED - aborted due to escalation
+		1900	ENDED_OK - ended normally
+		1902	ENDED_QUEUE_END
+		1903	ENDED_CONTAINER_END
+		1910	ENDED_EMPTY - task is empty (STOP NOMSG).
+		1911	ENDED_TRUNCATE - Transfer incomplete due to line limit.
+		1912	ENDED_EMPTY - nothing found.
+		1919	ENDED_INACTIVE - inactive because logical date condition of external dependency.
+		1920	ENDED_INACTIVE - inactive today because of Calendar.
+		1921	ENDED_INACTIVE - task not active due to definition
+		1922	ENDED_INACTIVE - Task was manually set inactive.
+		1930	ENDED_SKIPPED - Skipped because of WHEN clause
+		1931	ENDED_SKIPPED - Skipped because of SYNC condition.
+		1940	ENDED_TIMEOUT - Not executed because of timeout (WHEN clause).
+		1941	ENDED_TIMEOUT - Start time exceeded.
+		1942	ENDED_TIMEOUT - ended untimely.
+		1943	ENDING */
+	}
 }
