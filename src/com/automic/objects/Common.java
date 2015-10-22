eebb779fa2feb1c44d850beed434470158e5a04a
bsp <brendan.sapience@gmail.com>
2015-10-21 22:05:12 -0400

    adding search method

--- src/com/automic/objects/Common.java
+++ src/com/automic/objects/Common.java
@@ -243,6 +243,22 @@ public class Common extends ObjectTemplate{
 		}
 	return results;
 	}
+	public List<SearchResultItem> searchExecutableObjects(String ObjectName) throws IOException{
+		ObjectBroker broker = getBrokerInstance();
+		SearchObject ser = new SearchObject();
+		ser.unselectAllObjectTypes();
+		ser.setTypeExecuteable();
+		ser.setSearchLocation(broker.folders.getRootFolder().fullPath(), true);
+		ser.setName(ObjectName);
+		connection.sendRequestAndWait(ser);
+		Iterator<SearchResultItem> it =  ser.resultIterator();
+		List<SearchResultItem> results = new ArrayList<SearchResultItem>();
+		while(it.hasNext()){
+			SearchResultItem item = it.next();
+			results.add(item);
+		}
+	return results;
+	}
 	public List<SearchResultItem> searchJOBF(String ObjectName) throws IOException{
 		ObjectBroker broker = getBrokerInstance();
 		SearchObject ser = new SearchObject();
